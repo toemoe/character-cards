489cbfada34c10f1eb4faca62a7e3add99bde976
strangka <strangka@student.21-school.ru>
2025-04-01 13:54:06 +0500

    Updates

---
+++ _next/static/chunks/app/layout-b8ca619636608839.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2576:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3999,23))},3999:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[736,441,684,358],()=>s(2576)),_N_E=e.O()}]);
